87ef0e6ce8d336f9aaa3f96573ad5bdf506f21de
Nuraliev-Islam <islamnuraliev2020@gmail.com>
2025-03-30 22:57:34 +0300

    finich

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,50 +1,62 @@
+// Группировка по папкам для лучшей читаемости и удобства поддержки.
 export { Header } from './header/Header';
 export { Footer } from './footer/Footer';
-export { Breadcrumb } from "./breadcrumb/Breadcrumb"
+export { Breadcrumb } from "./breadcrumb/Breadcrumb";
+
+// HomePage компоненты
 export { HomeBanner } from './homePageComponents/HomeBanner/HomeBanner';
 export { HomeRating } from './homePageComponents/HomeRating/HomeRating';
 export { HomeViews } from './homePageComponents/HomeViews/HomeViews';
+export { AfishaEvent } from './homePageComponents/afishaEvent/AfishaEvent';
+export { HomeNews } from './homePageComponents/homeNews/HomeNews';
+
+// ReaderPage компоненты
 export { Banner } from "./readerPageComponents/banner/Banner";
 export { Catalog } from './readerPageComponents/catalog/Catalog';
 export { Graphik } from './readerPageComponents/graphik/Graphik';
 export { Video } from './readerPageComponents/video/Video';
+export { Rating } from './readerPageComponents/rating/Rating';
+
+// AboutPage компоненты
 export { AboutBanner } from './aboutPageComponents/aboutBanner/AboutBanner';
-export { Management } from './aboutPageComponents/management/Management'
-export { Structure } from './aboutPageComponents/structure/Structure'
-export { Activities } from './aboutPageComponents/activities/Activities'
-export { History } from './aboutPageComponents/history/History'
-export { AfishaDetailSection } from "./afishaSection/afishaDetailSections/AfishaDetailSection"
-export { AfishaBanner } from "./afishaSection/afishaBanner/AfishaBanner"
-export {AfishaEvents} from "./afishaSection/afishaEvents/AfishaEvents"
-export { AfishaEvent } from './homePageComponents/afishaEvent/AfishaEvent';
+export { Management } from './aboutPageComponents/management/Management';
+export { Structure } from './aboutPageComponents/structure/Structure';
+export { Activities } from './aboutPageComponents/activities/Activities';
+export { History } from './aboutPageComponents/history/History';
+
+// AfishaSection компоненты
+export { AfishaDetailSection } from "./afishaSection/afishaDetailSections/AfishaDetailSection";
+export { AfishaBanner } from "./afishaSection/afishaBanner/AfishaBanner";
+export { AfishaEvents } from "./afishaSection/afishaEvents/AfishaEvents";
+
+// OurProjects компоненты
 export { ProjectBanner } from './ourProjects/ProjectBanner/ProjectBanner';
 export { MainProjects } from './ourProjects/MainProject/MainProjects';
 export { AmericanCorner } from './ourProjects/AmericanCorner/AmericanCorner';
+export { OurProjectDetail } from './ourProjects/ourProjectDetail/OurProjectDetail';
 
-
+// SupportPage компоненты
 export { SupportBanner } from './supportPageComponents/supportBanner/SupportBanner';
 export { SupportPartners } from './supportPageComponents/supportPartners/SupportPartners';
 export { SupportPayment } from './supportPageComponents/supportPayment/SupportPayment';
 export { SupportValue } from './supportPageComponents/supportValue/SupportValue';
+
+// ElectronicKatalog компоненты
 export { ElectronicBanner } from './electronicKatalogComponents/electronicBanner/ElectronicBanner';
 export { ElectronicPartners } from './electronicKatalogComponents/electronicPartners/ElectronicPartners';
 export { ElectronicSearch } from './electronicKatalogComponents/electronicSearch/ElectronicSearch';
 
+// ProfessionalComponents
 export { ProfessionalCategory } from './professionalComponents/professionalCategory/ProfessionalCategory';
 export { ProfessionalSearch } from './professionalComponents/professionalSearch/ProfessionalSearch';
 export { ProfessionalBaner } from './professionalComponents/professionalBaner/ProfessionalBaner';
-// export { SearchPage } from "./professionalComponents/search/SearchPage";
-// export { SearchBar } from "./professionalComponents/searchBar/SearchBar";
 
-
+// CatalogSection компоненты
 export { CatalogBanner } from './catalogSection/catalogBanner/CatalogBanner';
 export { CatalogInput } from './catalogSection/catalogInput/CatalogInput';
-export {HomeNews}  from "./homePageComponents/homeNews/HomeNews"
-export {NewsBanner} from "./newsPageComponents/NewsBanner/NewsBanner"
-export { NewsBooksUpdate} from "./newsPageComponents/NewsBooksUpdate/NewsBooksUpdate"
-export {NewsAdvertisment} from "./newsPageComponents/NewsAdvertisment/NewsAdvertisment"
 
-export {Rating} from './readerPageComponents/rating/Rating';
-export {OurProjectDetail} from './ourProjects/ourProjectDetail/OurProjectDetail';
-
+// NewsPage компоненты
+export { NewsBanner } from "./newsPageComponents/NewsBanner/NewsBanner";
+export { NewsBooksUpdate } from "./newsPageComponents/NewsBooksUpdate/NewsBooksUpdate";
+export { NewsAdvertisment } from "./newsPageComponents/NewsAdvertisment/NewsAdvertisment";
 export { NewsDeteilSection } from './newsPageComponents/NewsDeteil/NewsDeteilSection';
